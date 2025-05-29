895f78b10138b87024c433b5e3b4bb110306eefb
Charles Scarborough <c.sryan@outlook.com>
2025-05-29 09:11:21 -0700

    Added functions to download to folder instead of into R

--- hms_smoke_fire_download.R
+++ hms_smoke_fire_download.R
@@ -58,3 +58,54 @@ fire_download_function <- function(date) {
   } else {message("Shapefile does not exist.")}
   return(fire_sf)
 }
+
+smoke_download_to_folder_function <- function(date, out_dir) {
+  if (is.na(try(as.Date(date, format = "%Y-%m-%d")))) {
+    cli::cli_abort("{.arg date} must be in `%Y-%m-%d` format as a character string (example: 2023-01-01).")
+  }
+  if (as.Date(date, format = "%Y-%m-%d") >= Sys.Date()) {
+    cli::cli_abort("{.arg date} must be less than the current date.")
+  }
+  if (httr::HEAD(paste0("https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/", 
+                        format(as.Date(date, format = "%Y-%m-%d"), "%Y"), "/",
+                        format(as.Date(date, format = "%Y-%m-%d"), "%m"), "/",
+                        "hms_smoke",
+                        gsub("-", "", as.character(date)),
+                        ".zip"))$status_code == 200) {
+    message(paste0("Downloading HMS smoke contours for ", date))
+    temp_shp_file <- tempfile(fileext = ".zip")
+    temp_shp_dir <- tempdir()
+    download.file(paste0("https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/", 
+                         format(as.Date(date, format = "%Y-%m-%d"), "%Y"), "/",
+                         format(as.Date(date, format = "%Y-%m-%d"), "%m"), "/",
+                         "hms_smoke",
+                         gsub("-", "", as.character(date)),
+                         ".zip"), destfile = temp_shp_file)
+    unzip(temp_shp_file, overwrite = T, exdir = out_dir)
+  } else {message("Shapefile does not exist.")}
+}
+fire_download_to_folder_function <- function(date, out_dir) {
+  if (is.na(try(as.Date(date, format = "%Y-%m-%d")))) {
+    cli::cli_abort("{.arg date} must be in `%Y-%m-%d` format as a character string (example: 2023-01-01).")
+  }
+  if (as.Date(date, format = "%Y-%m-%d") >= Sys.Date()) {
+    cli::cli_abort("{.arg date} must be less than the current date.")
+  }
+  if (httr::HEAD(paste0("https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/", 
+                        format(as.Date(date, format = "%Y-%m-%d"), "%Y"), "/",
+                        format(as.Date(date, format = "%Y-%m-%d"), "%m"), "/",
+                        "hms_fire",
+                        gsub("-", "", as.character(date)),
+                        ".zip"))$status_code == 200) {
+    message(paste0("Downloading HMS smoke contours for ", date))
+    temp_shp_file <- tempfile(fileext = ".zip")
+    temp_shp_dir <- tempdir()
+    download.file(paste0("https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/", 
+                         format(as.Date(date, format = "%Y-%m-%d"), "%Y"), "/",
+                         format(as.Date(date, format = "%Y-%m-%d"), "%m"), "/",
+                         "hms_fire",
+                         gsub("-", "", as.character(date)),
+                         ".zip"), destfile = temp_shp_file)
+    unzip(temp_shp_file, overwrite = T, exdir = out_dir)
+  } else {message("Shapefile does not exist.")}
+}
